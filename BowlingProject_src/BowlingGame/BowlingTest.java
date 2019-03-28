f9a0644ad824fa391f23cf24e8af6b1ccf7e8de8
edlerj <37121837+edlerj@users.noreply.github.com>
2019-03-28 14:58:04 -0500

    Refactored bowling game.

---
+++ BowlingProject_src/BowlingGame/BowlingTest.java
@@ -0,0 +1,102 @@
+package BowlingGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class BowlingTest {
+
+	private Game g;
+
+	private void rollMany(int n, int pins) {
+		for(int i = 0; i < n; i++) {
+			g.roll(pins);
+		}
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		g = new Game();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void canCreateGame() {
+
+	}
+	
+	@Test
+	public void canRoll() {
+		g.roll(0);
+	}
+	
+	@Test
+	public void gutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+	
+	@Test
+	public void allOnes() {
+		rollMany(20, 1);
+		assertEquals(20, g.score());	
+	}
+	
+	@Test
+	public void oneSpare() {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		//this isn't right!!
+		assertEquals(16,g.score());
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+	
+	@Test
+	public void oneStrike() {
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16,0);
+		assertEquals(24, g.score());
+	}
+	
+	@Test
+	public void fullGame() {
+		g.roll(6);
+		g.roll(2);
+		g.roll(7);
+		g.roll(2);
+		g.roll(3);
+		g.roll(4);
+		g.roll(8);
+		g.roll(2);
+		g.roll(9);
+		g.roll(0);
+		g.roll(10);
+		g.roll(10);
+		g.roll(10);
+		g.roll(6);
+		g.roll(3);
+		g.roll(8);
+		g.roll(2);
+		g.roll(7);
+		assertEquals(153, g.score());
+	}
+}
